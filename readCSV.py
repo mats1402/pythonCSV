ef3bee5e2162a46b454a5814fcc186b45624798d
mats1402 <mats1402@osloskolen.no>
2021-04-23 11:37:54 +0200

    woooo

--- readCSV.py
+++ readCSV.py
@@ -6,9 +6,9 @@ with open('testCSV.csv') as csv_file:
     line_count = 0
     for row in csv_reader:
         if line_count == 0:
-            print(f'Column names are {​​​​​​​",".join(row)}​​​​​​​​')
+            print(f'column names are{", ".join(row)}')
             line_count += 1
         else:
-            print(f'\t{​​​​​​​​row[0]}​​​​​​​​ Cost {​​​​​​​​row[1]}​​​​​​​​, Sold {​​​​​​​​row[2]}​​​​​​​​, Profit {​​​​​​​​row[3]}​​​​​​​​.')
-            line_count += 1
-    print(f'Processed {​​​​​​​​line_count}​​​​​​​​ lines.')
+            print(f'\t{row[0]} Cost {row[1]}, Sold {row[2]}, Profit {row[3]}.')
+            line_count +=1
+    print(f'Processed{line_count} lines.')
